088a5a39f997ca59c2abed467f58be2ab4931219
Mason <olsenm2828@gmail.com>
2020-11-07 17:15:12 -0800

    rough framework

---
+++ assignment.py
@@ -0,0 +1,7 @@
+class Assignment(object):
+
+	def __init__(name, percent, length, info):
+		self.name = name
+		self.worth = percent
+		self.time = length
+		self.notes = info
---
+++ schedule.py
@@ -0,0 +1,16 @@
+class Schedule(object):
+	"""docstring for Schedule"""
+	def __init__(self):
+		self.schedule = [[] * 7]
+		for i in range(len(self.schedule)):
+			self.schedule[i] = ["available"] * 24
+
+	def add_assignment(assignment):
+		return
+
+	def generate_schedule():
+		return
+
+	def add_break(time1, time2):
+		return 
+		
