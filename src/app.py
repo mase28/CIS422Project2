63d3165e3cd0612add6dabbdb3c08d83176c5ce1
Mapuana Maka <mmaka@uoregon.edu>
2020-11-09 12:01:39 -0800

    working on html pages

--- src/app.py
+++ src/app.py
@@ -1,4 +1,23 @@
 #Flask App file
-from flask import Flask 
+import os 
+from flask import Flask, render_template, request, redirect, url_for, 
 
 app = Flask(__name__)
+
+#Initial page
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/form_input', methods=['GET', 'POST'])
+def form_input():
+    if request.method == 'POST':
+        #do stuff when the form is submitted
+
+        # redirect to end the POST handling
+        #go to calendar
+        return redirect(url_for('calendar.html'))
+    return render_template('form_input.html')
+
+@app.route('/calendar')
+def 
